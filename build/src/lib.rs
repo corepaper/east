7a58437d186eabc958990ee36b52e1d35e6e1370
Wei Tang <wei@that.world>
2022-11-30 18:22:55 +0100

    Init east-build and east-build-helper

---
+++ build/src/lib.rs
@@ -0,0 +1,91 @@
+//! Build integration for East.
+
+pub use axum;
+pub use east;
+pub use include_dir;
+pub use mime;
+
+#[macro_export]
+macro_rules! include_trunk_assets {
+    {
+        Asset = $asset_ty:ident,
+        Html = $html_ty:ident,
+        TRUNK_ASSET_FILES = $asset_files:ident,
+        route_trunk_assets = $route_assets:ident,
+        replace_header = $replace_header:expr,
+        replace_body = $replace_body:expr,
+    } => {
+        static $asset_files: $crate::include_dir::Dir<'static> = $crate::include_dir::include_dir!("$OUT_DIR/dist");
+
+        /// Asset type.
+        pub struct $asset_ty {
+            pub ext: Option<String>,
+            pub content: Vec<u8>,
+        }
+
+        impl $crate::axum::response::IntoResponse for $asset_ty {
+            fn into_response(self) -> $crate::axum::response::Response {
+                let mime = match self.ext.as_ref().map(|s| s.as_ref()) {
+                    Some("html") => Some($crate::mime::TEXT_HTML),
+                    Some("css") => Some($crate::mime::TEXT_CSS),
+                    Some("js") => Some($crate::mime::TEXT_JAVASCRIPT),
+                    Some("wasm") => {
+                        Some($crate::mime::Mime::from_str("application/wasm").expect("wasm is valid mime"))
+                    }
+                    _ => None,
+                };
+
+                if let Some(mime) = mime {
+                    ($crate::axum::TypedHeader($crate::axum::headers::ContentType::from(mime)), self.content).into_response()
+                } else {
+                    self.content.into_response()
+                }
+            }
+        }
+
+        /// Route assets.
+        pub fn $route_assets(mut app: Router) -> Router {
+            for entry in $asset_files.entries() {
+                if let Some(file) = entry.as_file() {
+                    let ext = file
+                        .path()
+                        .extension()
+                        .and_then(|s| s.to_str())
+                        .map(|s| s.to_owned());
+                    let path = file.path().to_string_lossy();
+
+                    if path != "index.html" {
+                        let content = file.contents().to_vec();
+
+                        app = app.route(
+                            &("/".to_string() + &path),
+                            routing::get(move || async { $asset_ty { ext, content } }),
+                        );
+                    }
+                }
+            }
+
+            app
+        }
+
+        /// Html type.
+        #[derive(Debug, Clone)]
+        pub struct $html_ty {
+            pub header: $crate::east::Markup,
+            pub body: $crate::east::Markup,
+        }
+
+        impl $crate::axum::response::IntoResponse for $html_ty {
+            fn into_response(self) -> $crate::axum::response::Response {
+                let template_file = $asset_files.get_file("index.html").expect("index file exist");
+                let template = template_file.contents_utf8().expect("template is valid utf8");
+
+                $crate::axum::response::IntoResponse::into_response($crate::axum::response::Html(
+                    template
+                        .replace($replace_header, &self.header.0)
+                        .replace($replace_body, &self.body.0)
+                ))
+            }
+        }
+    }
+}
--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -1,50 +1,15 @@
-use axum::{
-    headers::ContentType,
-    response::{Html, IntoResponse, Response},
-    routing, Router, TypedHeader,
-};
-use east::{render, render_with_component, Markup};
+use axum::{routing, Router};
+use east::{render, render_with_component};
 use std::{net::SocketAddr, str::FromStr};
 use ui::{AnyComponent, Index};
 
-static UI_FILES: include_dir::Dir<'static> = include_dir::include_dir!("$OUT_DIR/dist");
-
-pub struct Asset {
-    ext: Option<String>,
-    content: Vec<u8>,
-}
-
-impl IntoResponse for Asset {
-    fn into_response(self) -> Response {
-        let mime = match self.ext.as_ref().map(|s| s.as_ref()) {
-            Some("html") => Some(mime::TEXT_HTML),
-            Some("css") => Some(mime::TEXT_CSS),
-            Some("js") => Some(mime::TEXT_JAVASCRIPT),
-            Some("wasm") => {
-                Some(mime::Mime::from_str("application/wasm").expect("wasm is valid mime"))
-            }
-            _ => None,
-        };
-
-        if let Some(mime) = mime {
-            (TypedHeader(ContentType::from(mime)), self.content).into_response()
-        } else {
-            self.content.into_response()
-        }
-    }
-}
-
-fn html(header: Markup, body: Markup) -> Html<String> {
-    let template_file = UI_FILES.get_file("index.html").expect("index file exist");
-    let template = template_file
-        .contents_utf8()
-        .expect("template is valid utf8");
-
-    Html(
-        template
-            .replace("<!-- header -->", &header.0)
-            .replace("<!-- body -->", &body.0),
-    )
+east_build::include_trunk_assets! {
+    Asset = Asset,
+    Html = Html,
+    TRUNK_ASSET_FILES = TRUNK_ASSET_FILES,
+    route_trunk_assets = route_trunk_assets,
+    replace_header = "<!-- header -->",
+    replace_body = "<!-- body -->",
 }
 
 #[tokio::main]
@@ -52,22 +17,7 @@ async fn main() {
     // build our application with a route
     let mut app = Router::new();
 
-    for entry in UI_FILES.entries() {
-        if let Some(file) = entry.as_file() {
-            let ext = file
-                .path()
-                .extension()
-                .and_then(|s| s.to_str())
-                .map(|s| s.to_owned());
-            let path = file.path().to_string_lossy();
-            let content = file.contents().to_vec();
-
-            app = app.route(
-                &("/".to_string() + &path),
-                routing::get(move || async { Asset { ext, content } }),
-            );
-        }
-    }
+    app = route_trunk_assets(app);
 
     app = app.route("/", routing::get(index));
 
@@ -80,13 +30,11 @@ async fn main() {
         .unwrap();
 }
 
-async fn index() -> Html<String> {
-    east::sycamore::utils::hydrate::with_hydration_context(|| {
-        html(
-            render! {
-                title { "Counter" }
-            },
-            render_with_component!(AnyComponent, { Index {} }),
-        )
-    })
+async fn index() -> Html {
+    Html {
+        header: render! {
+            title { "Counter" }
+        },
+        body: render_with_component!(AnyComponent, { Index {} }),
+    }
 }
